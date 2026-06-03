7dc18483d69134dd80358f7e6d5eb064f69fbafb
Tharidsawan <patji.nkwan@gmail.com>
2026-06-03 13:26:31 +0700

    fix: simplify prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,19 +1,5 @@
 import { defineConfig } from "prisma/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import "dotenv/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-  migrate: {
-    async adapter() {
-      const { Pool } = await import("pg");
-      const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-      });
-      return new PrismaPg(pool);
-    },
-  },
 });
